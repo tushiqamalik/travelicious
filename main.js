a872d075267584f6e934817950caf6f0c1eaff71
tushiqamalik <105580656+tushiqamalik@users.noreply.github.com>
2024-01-10 18:41:23 +0530

    commit

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".changeContent", {
-    strings: ["Engineer", "Frontend Developer", "Content Writer"],
-    typeSpeed : 50,
-    backSpeed : 50,
-    backDelay : 1000,
-    loop : true
-});
